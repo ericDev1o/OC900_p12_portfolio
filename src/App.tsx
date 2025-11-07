104429b88e49ecab7a44ed5865e6d1643a82bf87
Eric Faraut <eric.faraut@hotmail.fr>
2025-11-07 22:59:38 +0100

    TailwindCSS set up and App.css rewrite is ongoing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import './App.css';
-
 function App() {
   const path = `${import.meta.env.BASE_URL}`;
 
@@ -29,19 +27,136 @@ function App() {
       </section>
       <section>
         <h3>compétences</h3>
-        <img src={viteLogo} className='logo' alt='Vite logo' />
-        <img src={htmlLogo} className='logo' alt='HTML5 logo' />
-        <img src={cssLogo} className='logo' alt='CSS3 logo' />
-        <img src={scssLogo} className='logo' alt='SCSS logo' />
-        <img src={jestLogo} className='logo' alt='Jest logo' />
-        <img src={jsLogo} className='logo' alt='JavaScript logo' />
-        <img src={tsLogo} className='logo' alt='TypeScript logo' />
-        <img src={reactLogo} className= 'logo react' alt='React logo' />
-        <img src={reactRouterLogo} className='logo' alt='React Router logo' />
-        <img src={reduxLogo} className='logo' alt='Redux logo' />
-        <img src={lighthouseLogo} className='logo' alt='Lighthouse logo' />
-        <img src={gtmetrixLogo} className='logo' alt='GTMetrix logo' />
-        <img src={waveLogo} className='logo' alt='WAvE logo' />
+        <img 
+          src={viteLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='Vite logo' />
+        <img 
+          src={htmlLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='HTML5 logo' />
+        <img 
+          src={cssLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='CSS3 logo' />
+        <img 
+          src={scssLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='SCSS logo' />
+        <img 
+          src={jestLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='Jest logo' />
+        <img 
+          src={jsLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='JavaScript logo' />
+        <img 
+          src={tsLogo} 
+          className=' 
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='TypeScript logo' />
+        <img 
+          src={reactLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#61dafbaa]
+            animate-[spin_20s_linear_infinite]' 
+          alt='React logo' />
+        <img 
+          src={reactRouterLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300' 
+          alt='React Router logo' />
+        <img 
+          src={reduxLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='Redux logo' />
+        <img 
+          src={lighthouseLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='Lighthouse logo' />
+        <img 
+          src={gtmetrixLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='GTMetrix logo' />
+        <img 
+          src={waveLogo} 
+          className='
+            p-1.5rem 
+            h-3em 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]' 
+          alt='WAvE logo' />
       </section>
       <section>
         <h3>projets</h3>
@@ -59,11 +174,46 @@ function App() {
       <section>
         <h3>contact</h3>
         <a href='https://github.com/ericDev1o' target='_blank'>
-          <img src={githubLogo} className='logo' alt='GitHub logo' />
+          <img 
+            src={githubLogo} 
+            className='
+              p-1.5rem 
+              h-3em 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]' 
+            alt='GitHub logo' />
         </a>
         <a href='https://linkedin.com/in/ericfaraut' target='_blank'>
-          <img src={linkedinLogo} className='logo' alt='LinkedIn logo' />
+          <img 
+            src={linkedinLogo} 
+            className='
+              p-1.5rem 
+              h-3em 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]' 
+            alt='LinkedIn logo' />
         </a>
+        <form className="max-w-md mx-auto">
+          <div className="relative z-0 w-full mb-5 group">
+              <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+              <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+                <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+            </div>
+          </div>
+          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
       </section>
     </main>
   );
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss';
+export default {
+  important: true,
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  plugins: [
+    await import("./node_modules/flowbite/plugin.js").then(m => m.default)
+  ],
+} satisfies Config;

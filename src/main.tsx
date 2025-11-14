1a5947689b2dbf79d3812832b6eb13ed81106c7c
Eric Faraut <eric.faraut@hotmail.fr>
2025-11-14 12:54:48 +0100

    SPA bundle is lighter.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeInit } from '../.flowbite-react/init';
 
 import './index.css';
-import MyRouting from './MyRouting';
+import Home from './pages/Home';
+import Layout from './components/containers/Layout';
 
 const root = document.getElementById('root');
 if(root) {
   createRoot(root).render(
     <StrictMode>
-      <MyRouting />
+      <Layout>
+        <Home />
+      </Layout>
     </StrictMode>
   );
 }

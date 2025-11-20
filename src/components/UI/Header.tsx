49e5bd91ce56e4d02fe726f4669dba3bf7039ca4
Eric Faraut <eric.faraut@hotmail.fr>
2025-11-20 18:13:58 +0100

    vite update & light images are ok; ongoing html-validate.

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,53 +1,63 @@
 export default function Header() {
     const path = `${import.meta.env.BASE_URL}`;
-    const linkedinLogo = path + 'linkedin.svg';
-    const githubLogo = path + 'github.svg';
-    const myLogo = path + 'logo.svg';
 
-    return <header>
-        <nav className='
-            p-8 
-            flex
-            justify-between 
-            items-center'
-        >
-            <img 
-                src={myLogo} 
-                className=' 
-                    h-6 
-                    will-change-filter 
-                    filter 
-                    duration-300
-                    hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                alt='' />
-            <a 
-                href='https://github.com/ericDev1o' 
-                target='_blank'
+    const linkedinLogo = path + 'linkedin.webp';
+    const githubLogo = path + 'github.webp';
+    const myLogo = path + 'logo.webp';
+
+    return <>
+        <header>
+            <nav className='
+                p-8 
+                flex
+                justify-between 
+                items-center'
             >
-            <img 
-                src={githubLogo} 
-                className=' 
-                    h-6 
-                    will-change-filter 
-                    filter 
-                    duration-300
-                    hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                alt='GitHub logo' />
-            </a>
-            <a 
-                href='https://linkedin.com/in/ericfaraut' 
-                target='_blank'
-            >
-            <img 
-                src={linkedinLogo} 
-                className='
-                h-6 
-                will-change-filter 
-                filter 
-                duration-300
-                hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                alt='LinkedIn logo' />
-            </a>
-        </nav>
-    </header>
+                <img 
+                    src={myLogo} 
+                    className=' 
+                        h-6 
+                        will-change-filter 
+                        filter 
+                        duration-300
+                        hover:drop-shadow-[0_0_2em_#646cffaa]' 
+                    alt='' />
+                <a 
+                    href='https://github.com/ericDev1o' 
+                    target='_blank'
+                >
+                <img 
+                    src={githubLogo} 
+                    className=' 
+                        h-6 
+                        will-change-filter 
+                        filter 
+                        duration-300
+                        hover:drop-shadow-[0_0_2em_#646cffaa]' 
+                    alt='GitHub logo' />
+                </a>
+                <a 
+                    href='https://linkedin.com/in/ericfaraut' 
+                    target='_blank'
+                >
+                <img 
+                    src={linkedinLogo} 
+                    className='
+                        h-6 
+                        will-change-filter 
+                        filter 
+                        duration-300
+                        hover:drop-shadow-[0_0_2em_#646cffaa]' 
+                    alt='LinkedIn logo' />
+                </a>
+            </nav>
+        </header>
+        <hr 
+            className="
+                my-4 
+                h-px
+                border-0
+                bg-gray-600"
+        />
+    </>
 }

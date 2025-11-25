c4b4fa6fb743f25d2ead1d7142be50b3a0206768
Eric Faraut <eric.faraut@hotmail.fr>
2025-11-25 12:12:37 +0100

    Lighthouse results are ok.

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -17,7 +17,10 @@ export default function Footer() {
             filter 
             duration-300
             hover:drop-shadow-[0_0_2em_#646cffaa]' 
-            alt='Eric logo' />
+            alt='Eric logo' 
+            width={15}
+            height={24}
+          />
           <a 
             href='https://github.com/ericDev1o' 
             target='_blank'
@@ -25,14 +28,17 @@ export default function Footer() {
               m-10'
           >
             <img 
-                src={githubLogo} 
-                className=' 
-                h-6 
-                will-change-filter 
-                filter 
-                duration-300
-                hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                alt='GitHub logo' />
+              src={githubLogo} 
+              className=' 
+              h-6 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]' 
+              alt='GitHub logo' 
+              width={25}
+              height={24}
+            />
           </a>
           <a 
             href='https://linkedin.com/in/ericfaraut' 
@@ -41,14 +47,16 @@ export default function Footer() {
               m-10'
           >
             <img 
-                src={linkedinLogo} 
-                className='
-                h-6 
-                will-change-filter 
-                filter 
-                duration-300
-                hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                alt='LinkedIn logo' />
+              src={linkedinLogo} 
+              className='
+              h-6 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]' 
+              alt='LinkedIn logo' 
+              width={29}
+              height={24}/>
           </a>
       </footer>
 }
--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -21,7 +21,10 @@ export default function Header() {
                         filter 
                         duration-300
                         hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                    alt='Eric logo' />
+                    alt='Eric logo'
+                    width={15}
+                    height={24}
+                />
                 <a 
                     href='https://github.com/ericDev1o' 
                     target='_blank'
@@ -34,7 +37,9 @@ export default function Header() {
                         filter 
                         duration-300
                         hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                    alt='GitHub logo' />
+                    alt='GitHub logo'
+                    width={25}
+                    height={24} />
                 </a>
                 <a 
                     href='https://linkedin.com/in/ericfaraut' 
@@ -48,7 +53,10 @@ export default function Header() {
                         filter 
                         duration-300
                         hover:drop-shadow-[0_0_2em_#646cffaa]' 
-                    alt='LinkedIn logo' />
+                    alt='LinkedIn logo' 
+                    width={29}
+                    height={24}
+                />
                 </a>
             </nav>
         </header>

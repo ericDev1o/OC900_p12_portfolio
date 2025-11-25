3a4d41b1391c9adac1148f4c2fb10a0e3eee0f6d
Eric Faraut <eric.faraut@hotmail.fr>
2025-11-25 04:05:00 +0100

    pa11y and vitest-axe are reported. How? Private, secure, offline.

--- src/pages/Home.pa11y.test.ts
+++ src/pages/Home.pa11y.test.ts
@@ -1,7 +1,19 @@
 import pa11y from 'pa11y';
-import fs from 'fs/promises';
+import htmlReporter from 'pa11y-reporter-html';
+import fs from 'fs';
+import { writeFile } from 'fs/promises';
+import path from 'path';
 
-const results = pa11y('https://ericdev1o.github.io/OC900_p12_portfolio', {
-    reporter: 'html'
+describe('accessibility test', () => {
+    it('should generate pa11y report', async () => {
+        const results = await pa11y('https://ericdev1o.github.io/OC900_p12_portfolio');
+        const html = await htmlReporter.results(results);
+
+        const reportDir = path.resolve(__dirname, '../../reports');
+        if (!fs.existsSync(reportDir)) {
+        fs.mkdirSync(reportDir, { recursive: true });
+        }
+
+        await writeFile(path.resolve(reportDir, 'pa11y-report.html'), html, 'utf-8');
+    }, 30000);
 });
-await fs.writeFile('../../reports/pa11y-report.html', results, 'utf-8');

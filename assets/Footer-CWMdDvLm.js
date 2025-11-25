25032fe38e234490d5eea46d65d4c499a8b89254
Eric Faraut <eric.faraut@hotmail.fr>
2025-11-25 14:57:58 +0100

    Updates

---
+++ assets/Footer-CWMdDvLm.js
@@ -0,0 +1,21 @@
+import{j as e}from"./index-CO7sNgP2.js";function r(){return e.jsxs("footer",{className:`
+          flex 
+          justify-center`,children:[e.jsx("img",{src:"./logo.webp",className:` 
+            m-10 
+            h-6 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]`,alt:"Eric logo",width:15,height:24}),e.jsx("a",{href:"https://github.com/ericDev1o",target:"_blank",className:`
+              m-10`,children:e.jsx("img",{src:"./github.webp",className:` 
+              h-6 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]`,alt:"GitHub logo",width:25,height:24})}),e.jsx("a",{href:"https://linkedin.com/in/ericfaraut",target:"_blank",className:`
+              m-10`,children:e.jsx("img",{src:"./linkedin.webp",className:`
+              h-6 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]`,alt:"LinkedIn logo",width:29,height:24})})]})}export{r as default};

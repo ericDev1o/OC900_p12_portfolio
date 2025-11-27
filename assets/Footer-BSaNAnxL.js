7a96830b0470f259d5e7c06e94eb300d790c6a9d
Eric Faraut <eric.faraut@hotmail.fr>
2025-11-27 04:35:44 +0100

    Updates

---
+++ assets/Footer-BSaNAnxL.js
@@ -0,0 +1,21 @@
+import{j as t}from"./index-DfIdW2-K.js";function r(){const e="logos/",n="./"+e+"linkedin.webp",o="./"+e+"github.webp",a="./"+e+"logo.webp";return t.jsxs("footer",{className:`
+          flex 
+          justify-center`,children:[t.jsx("img",{src:a,className:` 
+            m-10 
+            h-6 
+            will-change-filter 
+            filter 
+            duration-300
+            hover:drop-shadow-[0_0_2em_#646cffaa]`,alt:"Eric logo",width:15,height:24}),t.jsx("a",{href:"https://github.com/ericDev1o",target:"_blank",className:`
+              m-10`,children:t.jsx("img",{src:o,className:` 
+              h-6 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]`,alt:"GitHub logo",width:25,height:24})}),t.jsx("a",{href:"https://linkedin.com/in/ericfaraut",target:"_blank",className:`
+              m-10`,children:t.jsx("img",{src:n,className:`
+              h-6 
+              will-change-filter 
+              filter 
+              duration-300
+              hover:drop-shadow-[0_0_2em_#646cffaa]`,alt:"LinkedIn logo",width:29,height:24})})]})}export{r as default};

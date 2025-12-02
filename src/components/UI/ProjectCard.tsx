66c386aa5587125d0f89413f33f810550e600eb4
Eric Faraut <eric.faraut@hotmail.fr>
2025-12-02 04:32:48 +0100

    ProjectCard details open/close smoothly.

--- src/components/UI/ProjectCard.tsx
+++ src/components/UI/ProjectCard.tsx
@@ -1,20 +1,15 @@
-import {useEffect, useRef, useState} from "react";
-
 import '../../types/Project';
+import AnimatedDetails from "./AnimatedDetails";
 
 export default function ProjectCard
 (
     {
-        index,
         project,
         basePath, 
-        logosPath, 
         projectsPath
     }: {
-        index: number, 
         project: Project
         basePath: string,
-        logosPath: string,
         projectsPath: string
     }
 )
@@ -22,28 +17,6 @@ export default function ProjectCard
     const alt = project.title.split(',')[0];
 
     const imgSrc = basePath + projectsPath + project.fileName;
-
-    const [openId, setOpenId] = useState<number | null>(null);
-
-    const isOpen = openId === project.number;
-
-    const detailsRef = useRef<(HTMLDetailsElement | null)[]>([]);
-
-    const handleToggle = (index: number, number: number) => {
-    const current = detailsRef.current[index];
-    if (current) {
-      if (openId === number) {
-        current.open = false;
-        setOpenId(null);
-      } else {
-        setOpenId(number);
-        detailsRef.current.forEach((detail, idx) => {
-          if (detail && idx !== index) detail.open = false;
-        });
-        current.open = true;
-      }
-    }
-  };
 
     return <article 
         className='
@@ -110,56 +83,19 @@ export default function ProjectCard
             </div>
         </a>
         <div className="space-y-4">
-            <details
-                key={project.number}
-                ref={(el) => {detailsRef.current[index] = el}}
-                open={isOpen}
-                onToggle={() => handleToggle(index, project.number)}
-                className="rounded shadow"
-            >
-                <summary className="
-                    list-none
-                    cursor-pointer 
-                    select-none 
-                    px-4 
-                    py-3 
-                    hover:bg-gray-700 
-                    hover:rounded
-                    font-semibold
-                    relative
-                    flex
-                    items-center
-                    justify-between"
-                >
-                    Découvrez ce projet
-                     <span
-                        className={`
-                            transition-transform
-                            duration-300
-                            ease-in-out
-                            inline-block ${
-                                isOpen ? 'rotate-180' : ''
-                             }
-                        `}
-                        aria-hidden="true"
-                        >
-                        ▼
-                        </span>
-                </summary>
-                <div className="px-4 py-3">
-                    <h6 className="font-extrabold mt-2">contexte</h6>
-                    <p>{project.contexte}</p>
-                    <h6 className="font-extrabold mt-2">objectifs</h6>
-                    <p>{project.objectifs}</p>
-                    <h6 className="font-extrabold mt-2">stack technique</h6>
-                    <p>{project.stackTechnique}</p><h6 className="font-extrabold mt-2">compétences développées</h6>
-                    <p>{project.competencesDeveloppees}</p>
-                    <h6 className="font-extrabold mt-2">résultats et impact</h6>
-                    <p>{project.resultatsEtImpact}</p>
-                    <h6 className="font-extrabold mt-2">perspectives d'amélioration</h6>
-                    <p>{project.perspectivesAmelioration}</p>
-                </div>
-            </details>
+            <AnimatedDetails summary="Découvrez ce projet">
+                <h6 className="font-extrabold mt-2">contexte</h6>
+                <p>{project.contexte}</p>
+                <h6 className="font-extrabold mt-2">objectifs</h6>
+                <p>{project.objectifs}</p>
+                <h6 className="font-extrabold mt-2">stack technique</h6>
+                <p>{project.stackTechnique}</p><h6 className="font-extrabold mt-2">compétences développées</h6>
+                <p>{project.competencesDeveloppees}</p>
+                <h6 className="font-extrabold mt-2">résultats et impact</h6>
+                <p>{project.resultatsEtImpact}</p>
+                <h6 className="font-extrabold mt-2">perspectives d'amélioration</h6>
+                <p>{project.perspectivesAmelioration}</p>
+            </AnimatedDetails>
         </div>
     </article>
 }

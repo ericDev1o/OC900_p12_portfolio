6bee63e99b48cab0995557faf26db43244a06c78
Eric Faraut <eric.faraut@hotmail.fr>
2025-12-11 14:25:51 +0100

    Missing link click warning in project card is added.

--- src/components/UI/SkillLogo.tsx
+++ src/components/UI/SkillLogo.tsx
@@ -3,13 +3,37 @@ export default function SkillLogo({ logoPath, repo}: {logoPath: string, repo: st
     const fileName = pathSplit[pathSplit.length - 1];
     const altName = fileName.split('.')[0];
     
-    return <section>
-        <a 
-            href={repo} 
-            target='_blank' 
-            aria-label={repo}
+    return <section
+        className='
+            group 
+            relative'
         >
-            <img src={logoPath} alt={altName}></img>
-        </a>
-    </section>
+            <a 
+                href={repo} 
+                target='_blank' 
+                aria-label={repo}
+            >
+                <img src={logoPath} alt={altName}></img>
+            </a>
+            <span className="
+                absolute 
+                bottom-full 
+                mb-2 
+                left-1/2 
+                -translate-x-1/2 
+                z-40 
+                bg-white 
+                text-gray-900 
+                text-xs 
+                rounded 
+                py-1 
+                px-3 
+                opacity-0 
+                group-hover:opacity-100 
+                transition-opacity
+                text-nowrap"
+            >
+                🔒 Vérifiez toujours l'URL avant de cliquer!
+            </span>
+        </section>
 }

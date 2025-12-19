72b3ec4b6937389f2f93450f5fffdcd8639c112f
Eric Faraut <eric.faraut@hotmail.fr>
2025-12-19 23:55:30 +0100

    Updates

--- assets/Footer-C85OHwec.js
+++ assets/Footer-C85OHwec.js
@@ -1,4 +1,4 @@
-import{r as o,j as e,H as t,L as a,M as r}from"./index-D5BPvyeu.js";function l(){const[n,s]=o.useState(!1);return o.useEffect(()=>(document.body.style.overflow=n?"hidden":"",()=>{document.body.style.overflow=""}),[n]),e.jsxs("footer",{className:`
+import{r as o,j as e,H as t,L as a,M as r}from"./index-CyrarAL2.js";function l(){const[n,s]=o.useState(!1);return o.useEffect(()=>(document.body.style.overflow=n?"hidden":"",()=>{document.body.style.overflow=""}),[n]),e.jsxs("footer",{className:`
             px-4 
             pb-15
             flex

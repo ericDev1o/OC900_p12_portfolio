5d6fe4de0703611c1f09b7e81abdd61b05a3f476
Eric Faraut <eric.faraut@hotmail.fr>
2026-01-07 13:53:25 +0100

    Code is cleaner: some unused vars and explicit any types are fixed.

--- src/vitest.d.ts
+++
@@ -1,7 +0,0 @@
-import 'vitest';
-import type { AxeMatchers } from 'vitest-axe/matchers';
-
-declare module 'vitest' {
-  interface Assertion extends AxeMatchers {}
-  interface AsymmetricMatchersContaining extends AxeMatchers {}
-}   

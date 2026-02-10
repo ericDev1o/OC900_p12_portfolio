8ba7510a098a4ba7ae6d0cb497a053f58e78445c
Eric Faraut <eric.faraut@hotmail.fr>
2026-02-10 18:26:12 +0100

    Footer legal modal is simplified and more a11y.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import Modal from 'react-modal';
-
 import { SkillsLogoProvider } from './contexts/SkillsLogoContext';
 
 import './index.css';
@@ -14,8 +12,6 @@ import Layout from './components/containers/Layout';
 
 const root = document.getElementById('root');
 if(root) {
-  Modal.setAppElement('#root');
-
   createRoot(root).render(
     <StrictMode>
       <SkillsLogoProvider>
